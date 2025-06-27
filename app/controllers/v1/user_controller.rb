86152f27ca819f0cc355c19079dd06a5210278f4
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-06-27 19:55:07 -0300

    add logic to call service and render json

--- app/controllers/v1/user_controller.rb
+++ app/controllers/v1/user_controller.rb
@@ -1,6 +1,17 @@
 module V1
   class UserController < ApplicationController
+    def check_status
+      ip = request.remote_ip
+      country = request.headers['CF-IPCountry']
 
+      service = UserStatusCheckerService.call(user_params: user_params, ip: ip, country: country)
+
+      if service.success?
+        render json: { ban_status: service.final_status_result }, status: :ok
+      else
+        render json: { error: 'Internal server error' }, status: :internal_server_error
+      end
+    end
 
     private
 
